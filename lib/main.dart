9211d1b2db19c37afd86528494d467217d98b523
Kinda Muhyeddin <kenda.mhd@gmail.com>
2024-09-24 17:47:38 +0300

    uptade

--- lib/main.dart
+++ lib/main.dart
@@ -11,63 +11,64 @@ class MyApp extends StatelessWidget {
     //const container = Container();
     return  MaterialApp(
       home:Scaffold(
-      backgroundColor: const Color.fromARGB(255, 98, 143, 98),
+      backgroundColor: const Color(0xFF1C694C),
       body: SafeArea(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               const CircleAvatar(
-                radius: 50.0,
+                radius: 70.0,
                 backgroundImage: AssetImage('images/myPhoto.jpeg'),
               ),
+              const SizedBox(height: 15,),
               const Text(
-                'كندة محي الدين',
+                'Kinda Muhyeddin',
                 style: TextStyle(
                   fontFamily: 'Harmattan',
-                  fontSize: 33.0 ,
-                  color: Colors.white,
+                  fontSize: 40.0 ,
+                  color: Color(0xFFF1F8E9),
                   fontWeight: FontWeight.bold,
                 ),
               ),
               Text(
-                'مهندسة كومبيوتر',
+                'Computer Engineer',
                 style: TextStyle(
-                  color: Colors.orange.shade50,
-                  fontSize: 20.0 ,
+                  color: Colors.lightGreen.shade100,
+                  fontSize: 25.0 ,
                   fontWeight: FontWeight.bold,),),
               SizedBox(
                 width: 200,
                 height: 30,
                 child: Divider(
-                  color: Colors.orange.shade50,
+                  color: Colors.lightGreen.shade100,
                 ),
                 ),
-              Card(
-                margin: const EdgeInsets.all(15),
+              const Card(
+                margin: EdgeInsets.all(15),
                 child: ListTile(
-                    leading: const Icon(
+                    leading: Icon(
                       Icons.phone,
-                      color: Color.fromARGB(255, 98, 143, 98),),
+                      color: Color.fromARGB(255, 28, 105, 76),),
                     title: Text(
                           '+90 551 954 75 88',
                           style: TextStyle(
                             fontSize: 20,
-                            color: Colors.green.shade800,
+                            color: Color.fromARGB(255, 28, 105, 76),
                       ),
                     )
                   ),
                 ),
-              Card(
-                margin: const EdgeInsets.all(15),
+              const Card(
+                margin: EdgeInsets.all(15),
                 child: ListTile(
-                  leading: const Icon(
+                  leading: Icon(
                     Icons.email,
-                    color: Color.fromARGB(255, 98, 143, 98),),
+                    color: Color.fromARGB(255, 28, 105, 76),),
                   title: Text(
                     'kenda.mhd@gmail.com',
                     style: TextStyle(
                     fontSize: 20,
-                    color: Colors.green.shade800,
+                    color: Color.fromARGB(255, 28, 105, 76),
                     ),
                   )
                 ),
